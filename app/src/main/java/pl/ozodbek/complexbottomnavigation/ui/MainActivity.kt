a478ed7449ac1749f9399e2de686c42514068f97
Ozodbek <ozodbekkarimov100@gmail.com>
2023-12-11 00:27:48 +0100

    second commit

--- app/src/main/java/pl/ozodbek/complexbottomnavigation/ui/MainActivity.kt
+++ app/src/main/java/pl/ozodbek/complexbottomnavigation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package pl.ozodbek.complexbottomnavigation
+package pl.ozodbek.complexbottomnavigation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.ozodbek.complexbottomnavigation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
